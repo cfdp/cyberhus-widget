2ca7f6a2214f8dd7c4d335f4616dba7d029024d4
Michael Lajlev <lajlev@gmail.com>
2016-10-27 20:19:44 +0200

    init

---
+++ data-forum.js
@@ -0,0 +1,23 @@
+forum([
+  {
+    "node" : {
+      "title" : "Hvordan begynder man?",
+      "date" : "2 dage 2 timer",
+      "path" : "https://cyberhus.dk/forum/seksualitet/hvordan-begynder-man"
+    }
+  },
+  {
+    "node" : {
+      "title" : "Sperm",
+      "date" : "3 dage 3 timer",
+      "path" : "https://cyberhus.dk/forum/seksualitet/sperm-0"
+    }
+  },
+  {
+    "node" : {
+      "title" : "Hvad betyder det?",
+      "date" : "3 dage 23 timer",
+      "path" : "https://cyberhus.dk/forum/migverden/hvad-betyder-det"
+    }
+  }
+])
---
+++ data-letterbox.js
@@ -0,0 +1,23 @@
+letterbox([
+  {
+    "node" : {
+      "title" : "Problemer med forhuden",
+      "date" : "1 dag 21 timer",
+      "path" : "https://cyberhus.dk/brevkasse/sp%C3%B8rgl%C3%A6gen/problemer-med-forhuden"
+    }
+  },
+  {
+    "node" : {
+      "title" : "Sperm",
+      "date" : "3 dage 3 timer",
+      "path" : "https://cyberhus.dk/brevkasse/seksualitet/sperm-9"
+    }
+  },
+  {
+    "node" : {
+      "title" : "Sur og frustreret over tilbage sigtelse af voldtægt?",
+      "date" : "3 dage 21 timer",
+      "path" : "https://cyberhus.dk/brevkasse/overgreb/sur-og-frustreret-over-tilbage-sigtelse-af-voldt%C3%A6gt"
+    }
+  }
+])
